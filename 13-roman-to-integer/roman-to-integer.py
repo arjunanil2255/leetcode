3132ec03fc7c72c655511ab9cb6ca4d836041aff
arjunanil2255 <158830074+arjunanil2255@users.noreply.github.com>
2026-08-08 00:32:52 +0530

    Time: 8 ms (25.69%) | Memory: 19.4 MB (19.17%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        rom = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+        for i in range(len(s)):
+            value = rom[s[i]]
+            if i + 1 < len(s) and value < rom[s[i + 1]]:
+                total -= value
+            else:
+                total += value
+        return total
+            
+                
+
+            
+
